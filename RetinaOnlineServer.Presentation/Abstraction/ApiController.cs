e528cd9cf7ae47ce00c41b1bb0d3c7f2a61d2cc1
sdkgk <sadik.gok@gmail.com>
2024-01-13 19:46:58 +0300

    Add project files.

---
+++ RetinaOnlineServer.Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetinaOnlineServer.Presentation.Abstraction
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+    }
+}
---
+++ RetinaOnlineServer.Presentation/Controller/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetinaOnlineServer.Presentation.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetinaOnlineServer.Presentation.Controller
+{
+    //sealed keywordu bu clasın başka bir clastan inherit edilmesini engeller
+    public sealed class TestController : ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(" işlem başarılı");
+        }
+    }
+}
