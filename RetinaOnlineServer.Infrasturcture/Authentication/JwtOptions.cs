718bf3a099bac1bc9ccba816a1c289723307520c
sdkgk <sadik.gok@gmail.com>
2024-01-16 23:17:56 +0300

    IoC Container configuration

--- RetinaOnlineServer.Infrasturcture/Authentication/JwtOptions.cs
+++ RetinaOnlineServer.Infrasturcture/Authentication/JwtOptions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace RetinaOnlineServer.Infrasturcture.Authentication
 {
@@ -11,10 +7,5 @@ namespace RetinaOnlineServer.Infrasturcture.Authentication
         public string Issuer { get; set; }
         public string Audience { get; set; }
         public string SecretKey { get; set; }
-
-        public static implicit operator JwtOptions(Microsoft.Extensions.Options.IOptions<JwtOptions> v)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
